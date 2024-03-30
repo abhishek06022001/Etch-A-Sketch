3a425fb4667a4656bf607858a4f6298b4562b026
Abhishek <satheabhishek1@gmail.com>
2024-03-30 15:18:03 +0530

    somewhat done

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-
-/**
- * select the button
- * prompt the user 
- * after prompting take input call make grid 
- * fill the grid 
- */
 const button = document.querySelector('#setGrid');
 button.addEventListener('click',function(){
+    // resizeTo()
+    resetGrid();
   let input = prompt("Input the grid rows u want!");
   makeGrid(input);
 })
-const container = document.querySelector('.grid');
+const container = document.querySelector('.container');
 function makeGrid(num)
 {
     for(let i = 0 ; i < num ; i++)
@@ -22,9 +17,12 @@ function makeGrid(num)
             let square = document.createElement('div');
             square.classList.add('square');
             square.style.flexGrow ="1";
-    square.style.width = '${560/input}px';
-    square.style.height = '${560/input}px';
-    square.style.backgroundColor = "pink";
+            square.style.width =`${560/num}px`;
+            square.style.height = `${560/num}px`;
+            square.style.backgroundColor = "pink";
+            square.style.border="0.00000011rem solid white";
+            square.style.boxSizing = "border-box";
+
             
             row.appendChild(square);
 
@@ -34,4 +32,12 @@ function makeGrid(num)
 
     }
     
+}
+function resetGrid()
+{
+ // 
+const squares = document.querySelectorAll('.square');
+squares.forEach(element => {
+    element.remove();
+});
 }
